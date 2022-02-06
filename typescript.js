ee3d8af71ec8b094ca188e2ed681b6839d204041
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2022-02-06 14:44:51 -0700

    feat: add initial lint config for TypeScript

---
+++ typescript.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "extends": [
+    "./javascript",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "rules": {
+    "no-shadow": "off",
+    "jsdoc/require-param-type": "off",
+    "jsdoc/require-property-type": "off",
+    "jsdoc/require-returns-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        "selector": "enumMember",
+        "format": ["PascalCase"]
+      }
+    ]
+  }
+};
