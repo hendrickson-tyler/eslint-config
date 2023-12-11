97d926ba1a101dab3642422053d1222c9d9fe2ca
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2023-12-10 17:23:40 -0700

    feat: remove chatty cyclomatic complexity template warning

--- angular-html.js
+++ angular-html.js
@@ -9,7 +9,6 @@ module.exports = {
     "@angular-eslint/template/table-scope": "error",
     "@angular-eslint/template/valid-aria": "error",
     "@angular-eslint/template/conditional-complexity": "warn",
-    "@angular-eslint/template/cyclomatic-complexity": "warn",
     "@angular-eslint/template/no-any": "error",
     "@angular-eslint/template/no-autofocus": "error",
     "@angular-eslint/template/no-call-expression": "warn",
